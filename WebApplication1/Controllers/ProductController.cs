58812c427bad77a54b178526da7f44515ff83b16
cg <pasoolee@outlook.com>
2018-08-15 15:49:16 -0400

    1 add first page contents of manual

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Api.Dtos;
+using WebApplication1.Api.Services;
+
+namespace WebApplication1.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet("all")]
+        public JsonResult GetProducts()
+        {
+            return new JsonResult(ProductService.Current.Products);
+        }
+        [Route("{id}")]
+        public JsonResult GetProduct(int id)
+        {
+            return new JsonResult(ProductService.Current.Products.SingleOrDefault(x => x.Id == id));
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Api.Dtos;
+
+namespace WebApplication1.Api.Services
+{
+    public class ProductService
+    {
+        public static ProductService Current { get; } = new ProductService();
+        public List<Product> Products { get; }
+        private ProductService()
+        {
+            Products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "milk",
+                    Price = 2.5f
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "bread",
+                    Price = 4.5f
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "beer",
+                    Price = 7.5f
+                }
+
+             };
+        }
+    }
+}
